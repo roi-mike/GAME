4bc8907309313d79f889fd28203ba5ab53754881
Maboza Hondi <dev.mabozahondi@gmail.com>
2021-02-04 19:32:15 +0100

    TP BACK END IFOCOP

--- serve.js
+++ serve.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const servePort = process.env.PORT;
+const servePort = process.env.PORT || 8080;
 app.use('/static', express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 
@@ -9,7 +9,7 @@ const postsRoutes = require('./routes/postsControler');
 
 app.use('/',postsRoutes);
 
-var serveport = app.listen(8080 || servePort , ()=>{
+var serveport = app.listen(servePort , ()=>{
     console.log('http://',serveport.address().address,':',serveport.address().port);
 });
  
